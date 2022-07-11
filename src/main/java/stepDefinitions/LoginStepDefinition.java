08a4e0f4878aef0a169c8d24233220622a5a5619
Rana <ranadheerchinna92@gmail.com>
2022-07-11 00:57:45 -0500

    updated xpaths

--- src/main/java/stepDefinitions/LoginStepDefinition.java
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -12,78 +12,77 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class LoginStepDefinition{
+public class LoginStepDefinition {
 
-	 WebDriver driver;
+    WebDriver driver;
 
-	
-	 @Given("^user is already on Login Page$")
-	 public void user_already_on_login_page(){
-	 System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
-	 driver = new ChromeDriver();
-	 driver.get("https://www.freecrm.com/index.html");
-	 }
-	
-	
-	 @When("^title of login page is Free CRM$")
-	 public void title_of_login_page_is_free_CRM(){
-	 String title = driver.getTitle();
-	 System.out.println(title);
-	 Assert.assertEquals("#1 Free CRM customer relationship management software cloud", title);
-	 }
-	
-	 //Reg Exp:
-	 //1. \"([^\"]*)\"
-	 //2. \"(.*)\"
-	
-	 @Then("^user enters \"(.*)\" and \"(.*)\"$")
-	 public void user_enters_username_and_password(String username, String password){
-		 driver.findElement(By.xpath("//span[normalize-space()='Log In']")).click();
-		 driver.findElement(By.xpath("//input[@placeholder='E-mail address']")).sendKeys(username);
-	 driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
-	 }
-	
-	 @Then("^user clicks on login button$")
-	 public void user_clicks_on_login_button() {
-	 WebElement loginBtn =
-	 driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']"));
-	 JavascriptExecutor js = (JavascriptExecutor)driver;
-	 js.executeScript("arguments[0].click();", loginBtn);
-	 }
-	
-	
-	 @Then("^user is on home page$")
-	 public void user_is_on_hopme_page(){
-	 String title = driver.getTitle();
-	 System.out.println("Home Page title ::"+ title);
-	 Assert.assertEquals("CRMPRO", title);
-	 }
-	 
-	 @Then("^user moves to new contact page$")
-	 public void user_moves_to_new_contact_page() {
-		driver.switchTo().frame("mainpanel");
-		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"))).build().perform();
-		driver.findElement(By.xpath("//a[contains(text(),'New Contact')]")).click();
-		
-		}
-	 
-	 
-	 @Then("^user enters contact details \"(.*)\" and \"(.*)\" and \"(.*)\"$")
-	 public void user_enters_contacts_details(String firstname, String lastname, String position){
-		 driver.findElement(By.id("first_name")).sendKeys(firstname);
-		 driver.findElement(By.id("surname")).sendKeys(lastname);
-		 driver.findElement(By.id("company_position")).sendKeys(position);
-		 driver.findElement(By.xpath("//input[@type='submit' and @value='Save']")).click();
-	 }
-	 
 
-	 @Then("^Close the browser$")
-	 public void close_the_browser(){
-		 driver.quit();
-	 }
-	
-	
-	
+    @Given("^user is already on Login Page$")
+    public void user_already_on_login_page() {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.freecrm.com/index.html");
+    }
+
+
+    @When("^title of login page is Free CRM$")
+    public void title_of_login_page_is_free_CRM() {
+        String title = driver.getTitle();
+        System.out.println(title);
+        Assert.assertEquals("Free CRM software for customer relationship management, sales, marketing campaigns and support.", title);
+    }
+
+    //Reg Exp:
+    //1. \"([^\"]*)\"
+    //2. \"(.*)\"
+
+    @Then("^user enters \"(.*)\" and \"(.*)\"$")
+    public void user_enters_username_and_password(String username, String password) throws InterruptedException {
+        driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@placeholder='E-mail address']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
+    }
+
+    @Then("^user clicks on login button$")
+    public void user_clicks_on_login_button() {
+        WebElement loginBtn =
+                driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", loginBtn);
+    }
+
+
+    @Then("^user is on home page$")
+    public void user_is_on_hopme_page() {
+        String title = driver.getTitle();
+        System.out.println("Home Page title ::" + title);
+        Assert.assertEquals("CRMPRO", title);
+    }
+
+    @Then("^user moves to new contact page$")
+    public void user_moves_to_new_contact_page() {
+        driver.switchTo().frame("mainpanel");
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"))).build().perform();
+        driver.findElement(By.xpath("//a[contains(text(),'New Contact')]")).click();
+
+    }
+
+
+    @Then("^user enters contact details \"(.*)\" and \"(.*)\" and \"(.*)\"$")
+    public void user_enters_contacts_details(String firstname, String lastname, String position) {
+        driver.findElement(By.id("first_name")).sendKeys(firstname);
+        driver.findElement(By.id("surname")).sendKeys(lastname);
+        driver.findElement(By.id("company_position")).sendKeys(position);
+        driver.findElement(By.xpath("//input[@type='submit' and @value='Save']")).click();
+    }
+
+
+    @Then("^Close the browser$")
+    public void close_the_browser() {
+        driver.quit();
+    }
+
 
 }
